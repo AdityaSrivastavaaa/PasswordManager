089679da58c6a162ebf561303c015dc205db98a5
AdityaSrivastavaaa <srivastavaaditya015@gmail.com>
2024-06-03 22:59:52 +0530

    saving in console is done

--- src/components/Manager.jsx
+++ src/components/Manager.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import add from "/icons8-add.gif"
 import show from "/show.png"
-import { useRef } from 'react'
+import { useRef  , useState} from 'react'
+
 const Manager = () => {
   const ref = useRef();
   const showPassword =()=> {
@@ -12,9 +13,16 @@ const Manager = () => {
       ref.current.src = "/hide.png"
      }
   }
+ const [form, setform] = useState({site:"",username:"",password:""})
+ const savePassword = () =>{
+  console.log(form)
+ }
+ const handleChange =(e)=>{
+   setform({...form , [e.target.name]:e.target.value})
+ }
   return (
     <div>
-      <div class="absolute inset-0 -z-10 h-full w-full bg-white [background:radial-gradient(125%_125%_at_50%_10%,#fff_40%,#63e_100%)]"></div>
+      <div className="absolute inset-0 -z-10 h-full w-full bg-white [background:radial-gradient(125%_125%_at_50%_10%,#fff_40%,#63e_100%)]"></div>
     
     <div className=' myContainer ' >
       <div className='text-purple-500 text-4xl font-bold text-center'>
@@ -23,17 +31,17 @@ const Manager = () => {
       
       <p className='text-lg font-bold text-center'>Your Own Password Manager</p>
       <div className='text-black flex flex-col p-4 gap-8 items-center'>
-           <input placeholder='Enter Website URL' className='rounded-full border border-purple-500 w-full p-4 py-1' type="text" name='' id='' />
+           <input value={form.site}  onChange={handleChange} placeholder='Enter Website URL' className='rounded-full border border-purple-500 w-full p-4 py-1' type="text" name='site' id='' />
            <div className='flex w-full gap-8'>
-           <input placeholder='Enter username' className='rounded-full border border-purple-500 w-full p-4 py-1' type="text" name='' id='' />
+           <input value={form.username}  onChange={handleChange} placeholder='Enter username' className='rounded-full border border-purple-500 w-full p-4 py-1' type="text" name='username' id='' />
            <div className="relative flex justify-center items-center ">
-           <input placeholder='Enter Password' className='rounded-full border border-purple-500 w-full p-6 py-1' type="text" name='' id='' />
+           <input value={form.password}  onChange={handleChange} placeholder='Enter Password' className='rounded-full border border-purple-500 w-full p-6 py-1' type="text" name='password' id='' />
            <span className='absolute right-2 top-1 cursor-pointer' onClick={showPassword}><img width={25}  ref={ref} src={show} alt="" /></span>
            </div>
            
 
            </div>
-           <button className='flex justify-center items-center rounded-full px-5 py-3  w-fit bg bg-purple-400 hover:bg-purple-300 gap-2 border border-purple-900'>
+           <button onClick={savePassword} className='flex justify-center items-center rounded-full px-5 py-3  w-fit bg bg-purple-400 hover:bg-purple-300 gap-2 border border-purple-900'>
           <img src={add} className='w-6 ' alt="" /> Add Password</button>
       </div>
      
